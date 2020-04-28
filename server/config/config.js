11b5dff257ecbabcebcadf9c621f142b1bc16e5d
Jonathan <cutuquimbi2@gmail.com>
2020-04-28 15:28:45 -0300

    Variable de entorno pagina mongo

--- server/config/config.js
+++ server/config/config.js
@@ -2,11 +2,11 @@ process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-//let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
-//    urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://Cutuquimbi:9CE4Q9kBADGXZk1d@cluster0-ubxft.mongodb.net/cafe';
-//}
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.NODE_URL;
+}
 
 process.env.URLDB = urlDB;
